c2279f661434a34f5ef7dbc268c3ea8eb1ee1d68
Harper Rhett <harperrhett@gmail.com>
2025-08-18 11:49:11 -0500

    Added project

---
+++ HarpEngine/Engine.cs
@@ -0,0 +1,20 @@
+﻿global using static Raylib_cs.Raylib;
+global using static Raylib_cs.Raymath;
+
+namespace HarpEngine;
+
+public static class Engine
+{
+	public static void Start(Runtime runtime)
+	{
+		
+	}
+
+	private static void Update()
+	{
+		while (!WindowShouldClose())
+		{
+
+		}
+	}
+}
