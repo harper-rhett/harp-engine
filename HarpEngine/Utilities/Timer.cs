7ddb858dd2c8e30eda25b21f9287323ed6a81e8c
Harper Rhett <harperrhett@gmail.com>
2025-08-21 11:12:35 -0500

    Timer changes

--- HarpEngine/Utilities/Timer.cs
+++ HarpEngine/Utilities/Timer.cs
@@ -22,3 +22,27 @@ public class FireTimer : Entity
 
 	public override void Draw() { }
 }
+
+public class TriggerTimer : Entity
+{
+	private float triggerTime;
+	private float startTime;
+	public Action Triggered;
+
+	public TriggerTimer(Scene scene, float triggerTime) : base(scene)
+	{
+		this.triggerTime = triggerTime;
+		startTime = scene.Time;
+	}
+
+	public override void Update(float frameTime)
+	{
+		if (scene.Time >= startTime + triggerTime)
+		{
+			Triggered();
+			scene.RemoveEntity(this);
+		}
+	}
+
+	public override void Draw() { }
+}
