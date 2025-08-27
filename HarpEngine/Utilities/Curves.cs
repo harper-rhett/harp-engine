90906636acad05aa1b1b4e1457fe4c57e8bb6b2f
Harper Rhett <harperrhett@gmail.com>
2025-08-26 21:46:55 -0500

    rudimentary easing

---
+++ HarpEngine/Utilities/Curves.cs
@@ -0,0 +1,59 @@
+﻿namespace HarpEngine.Utilities;
+
+// Some of these functions are sourced from https://easings.net/
+// I would recommend checking it out for more curves
+
+public static class Curves
+{
+	public static float Linear(float x) => x;
+
+	public static float SmoothStep(float x)
+	{
+		return x * x * (3f - 2f * x);
+	}
+
+	public static float SuperSmoothStep(float x)
+	{
+		return x * x * x * (x * (6f * x - 15f) + 10f);
+	}
+
+	public static float Cubic(float x)
+	{
+		return x * x * x;
+	}
+
+	public static float CubicInverse(float x)
+	{
+		return 1f - float.Pow(1f - x, 3f);
+	}
+
+	public static float CubicUniform(float x)
+	{
+		return x < 0.5f ? 4f * x * x * x : 1f - float.Pow(-2f * x + 2f, 3f) / 2f;
+	}
+
+	public static float Elastic(float x)
+	{
+		const float baseMultiplier = 1.70158f;
+		const float finalMultiplier = baseMultiplier * 1.525f;
+
+		return x < 0.5
+		  ? (float.Pow(2f * x, 2f) * ((finalMultiplier + 1f) * 2f * x - finalMultiplier)) / 2f
+		  : (float.Pow(2f * x - 2f, 2f) * ((finalMultiplier + 1f) * (x * 2f - 2f) + finalMultiplier) + 2f) / 2f;
+	}
+
+	public static float Bounce(float x)
+	{
+		const float bounceMultiplier = 7.5625f;
+		const float bounceDivisor = 2.75f;
+
+		if (x < 1f / bounceDivisor)
+			return bounceMultiplier * x * x;
+		else if (x < 2f / bounceDivisor)
+			return bounceMultiplier * (x -= 1.5f / bounceDivisor) * x + 0.75f;
+		else if (x < 2.5f / bounceDivisor)
+			return bounceMultiplier * (x -= 2.25f / bounceDivisor) * x + 0.9375f;
+		else
+			return bounceMultiplier * (x -= 2.625f / bounceDivisor) * x + 0.984375f;
+	}
+}
---
+++ HarpEngine/Utilities/Easer.cs
@@ -0,0 +1,73 @@
+﻿namespace HarpEngine.Utilities;
+
+public class Easer : Entity
+{
+	// General
+	private float startTime;
+	private float finishTime;
+	private float easeTime;
+
+	// Interface
+	public delegate float CurveFunction(float valueToCurve);
+	public CurveFunction Curve = Curves.Linear;
+	public Action Finished;
+	public bool IsEasing { get; private set; }
+	public float Progress { get; private set; }
+	public float CurveProgress { get; private set; }
+	public bool RemoveOnFinish = true;
+
+	public Easer(Scene scene, float easeTime) : base(scene)
+	{
+		this.easeTime = easeTime;
+	}
+
+	public override void Update(float frameTime)
+	{
+		if (!IsEasing) return;
+
+		if (scene.Time < finishTime) Ease();
+		else Finish();
+	}
+
+	public override void Draw() { }
+
+	private void Ease()
+	{
+		Progress = (scene.Time - startTime) / easeTime;
+		CurveProgress = Curve(Progress);
+	}
+
+	public void Start()
+	{
+		startTime = scene.Time;
+		finishTime = startTime + easeTime;
+		IsEasing = true;
+	}
+
+	private void Finish()
+	{
+		IsEasing = false;
+		Finished?.Invoke();
+		Remove();
+	}
+
+	public float EaseFloat(float from, float to)
+	{
+		return float.Lerp(from, to, CurveProgress);
+	}
+
+	public Vector2 EaseVector2(Vector2 from, Vector2 to)
+	{
+		return Vector2.Lerp(from, to, CurveProgress);
+	}
+
+	public Vector3 EaseVector3(Vector3 from, Vector3 to)
+	{
+		return Vector3.Lerp(from, to, CurveProgress);
+	}
+
+	public Quaternion EaseQuaternion(Quaternion from, Quaternion to)
+	{
+		return Quaternion.Lerp(from, to, CurveProgress);
+	}
+}
