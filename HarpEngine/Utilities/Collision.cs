b1e03e843c1e2dd1de1d10adb56903f6342280d6
Harper Rhett <harperrhett@gmail.com>
2025-08-29 13:30:26 -0500

    some collision code

---
+++ HarpEngine/Utilities/Collision.cs
@@ -0,0 +1,22 @@
+﻿namespace HarpEngine.Utilities;
+
+public static class Collision
+{
+	public static bool PointInCircle(Vector2 pointPosition, Vector2 circlePosition, float radiusSquared)
+	{
+		float distanceSquared = Vector2.DistanceSquared(pointPosition, circlePosition);
+		return distanceSquared < radiusSquared;
+	}
+
+	public static bool CircleOnCircle(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
+	{
+		float distanceSquared = Vector2.DistanceSquared(positionA, positionB);
+		return distanceSquared < radiusSquaredA + radiusSquaredB;
+	}
+
+	public static bool CircleInCirlce(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
+	{
+		float distanceSquared = Vector2.DistanceSquared(positionA, positionB);
+		return distanceSquared < radiusSquaredB - radiusSquaredA;
+	}
+}
