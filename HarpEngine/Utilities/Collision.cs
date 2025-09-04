0436467b86e2b54aec0d7eeaaff07df02b1197b6
Harper Rhett <harperrhett@gmail.com>
2025-09-04 13:58:45 -0500

    collision clarification

--- HarpEngine/Utilities/Collision.cs
+++ HarpEngine/Utilities/Collision.cs
@@ -2,19 +2,19 @@
 
 public static class Collision
 {
-	public static bool PointInCircle(Vector2 pointPosition, Vector2 circlePosition, float radiusSquared)
+	public static bool PointInCircleSquared(Vector2 pointPosition, Vector2 circlePosition, float radiusSquared)
 	{
 		float distanceSquared = Vector2.DistanceSquared(pointPosition, circlePosition);
 		return distanceSquared < radiusSquared;
 	}
 
-	public static bool CircleOnCircle(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
+	public static bool CircleOnCircleSquared(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
 	{
 		float distanceSquared = Vector2.DistanceSquared(positionA, positionB);
 		return distanceSquared < radiusSquaredA + radiusSquaredB;
 	}
 
-	public static bool CircleInCirlce(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
+	public static bool CircleInCircleSquared(Vector2 positionA, float radiusSquaredA, Vector2 positionB, float radiusSquaredB)
 	{
 		float distanceSquared = Vector2.DistanceSquared(positionA, positionB);
 		return distanceSquared < radiusSquaredB - radiusSquaredA;
