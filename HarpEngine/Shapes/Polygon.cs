679a0ef90acebf0c9f5d362aad7baf48c0366c24
Harper Rhett <harperrhett@gmail.com>
2025-09-05 09:20:10 -0500

    polygon shape

---
+++ HarpEngine/Shapes/Polygon.cs
@@ -0,0 +1,23 @@
+﻿namespace HarpEngine.Shapes;
+
+public class Polygon : Entity
+{
+	// General
+	public Vector2 Position;
+	public int SideCount;
+	public float Radius;
+	public Color Color;
+	public float Rotation;
+
+	public Polygon(Scene scene, float radius, int sideCount, Color color) : base(scene)
+	{
+		this.Radius = radius;
+		this.SideCount = sideCount;
+		this.Color = color;
+	}
+
+	public override void Draw()
+	{
+		Primitives.DrawPolygon(Position, SideCount, Radius, Rotation, Color);
+	}
+}
